d3ba47cbdeb7ed780d15b8421f9c8748285acfcb
phuongthaodoo <makymilesipun0suy@hotmail.com>
2024-05-07 21:19:58 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function findItinerary(tickets) {
-  const graph = new Map();
-  for (const [from, to] of tickets) {
-    if (!graph.has(from)) graph.set(from, []);
-    graph.get(from).push(to);
-  }
-  for (const destinations of graph.values()) {
-    destinations.sort();
-  }
-  const result = [];
-  dfs("JFK");
-  return result.reverse();
-  function dfs(from) {
-    const destinations = graph.get(from);
-    while (destinations && destinations.length) {
-      dfs(destinations.shift());
-    }
-    result.push(from);
-  }
+function LRUCache(capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
 }
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+  const value = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, value);
+  return value;
+};
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) this.cache.delete(key);
+  if (this.cache.size === this.capacity) {
+    const firstKey = this.cache.keys().next().value;
+    this.cache.delete(firstKey);
+  }
+  this.cache.set(key, value);
+};
